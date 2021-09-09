870de85e50e12131f4227ab5550b3a248908faf2
Evgeniya Kirtoka <evgeniyaKirtoka@gmail.com>
2021-09-09 17:50:56 +0300

    Fix user resolver

--- src/server/src/graphql/resolvers/user.ts
+++ src/server/src/graphql/resolvers/user.ts
@@ -1,19 +1,18 @@
-import { IUser, userInputSchema } from './../../model/user'
+import {IUser, userInputSchema} from '../../model/user'
 import Model from '../../model'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-const { User } = Model
 
-const { JWT_KEY = 'secret_key' } = process.env
+const {User} = Model
+
+const {JWT_KEY = 'secret_key'} = process.env
 
 export default {
   createUser: async ({
-    userInput: { login, password }
-  }: userInputSchema): Promise<string> => {
+                       userInput: {login, password}
+                     }: userInputSchema): Promise<{ token: string }> => {
     try {
-      console.log('FROM USER CREATE')
-
-      const existingUser = await User.findOne({ login })
+      const existingUser = await User.findOne({login})
 
       if (existingUser) {
         throw new Error('User exists already.')
@@ -23,26 +22,23 @@ export default {
         login,
         password
       })
-      const user: IUser = await preparedUser.save()
-      console.log('USER', user)
-      const token: string = jwt.sign(
-        { userId: user.id, login: user.login },
-        JWT_KEY,
-        { expiresIn: '1h' }
-      )
-      console.log('token', token)
 
-      return {token:to}
-      // return user
+      const user: IUser = await preparedUser.save()
+
+      const token: string = jwt.sign(
+        {userId: user.id, login: user.login},
+        JWT_KEY,
+        {expiresIn: '1h'}
+      )
+
+      return {token}
     } catch (error) {
       console.log(error)
       throw error
     }
   },
-  login: async ({
-    userInput: { login, password }
-  }: userInputSchema): Promise<string> => {
-    const user: IUser | null = await User.findOne({ login })
+  login: async ({userInput: {login, password}}: userInputSchema): Promise<{ token: string }> => {
+    const user: IUser | null = await User.findOne({login})
     if (!user) {
       throw new Error('User does not exist!')
     }
@@ -50,9 +46,10 @@ export default {
     if (!isEqual) {
       throw new Error('Password is incorrect!')
     }
-    const token: string = jwt.sign({ userId: user.id, login: login }, JWT_KEY, {
-      expiresIn: '1h'
-    })
-    return token
+    return {
+      token: jwt.sign({userId: user.id, login: login}, JWT_KEY, {
+        expiresIn: '1h'
+      })
+    }
   }
-}
+};
